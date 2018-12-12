bda2be11d64f5de7769b809d0c19196a0f797303
Jennifer Thompson <thompson.jennifer@gmail.com>
2018-12-11 18:46:22 -0600

    Start checking issues for enrollment qualification
    Shell for inclusion section in place

--- ih_dataclean.R
+++ ih_dataclean.R
@@ -3,6 +3,10 @@
 ##  collection
 ################################################################################
 
+## load tidyverse, lubridate for data management
+library(tidyverse)
+library(lubridate)
+
 ## -- Source helper functions --------------------------------------------------
 source("R/dataclean_helpers.R")
 
@@ -12,7 +16,7 @@ rc_url <- "https://redcap.vanderbilt.edu/api/"
 
 ## -- Read in data dictionary for in-hospital project --------------------------
 ## Will be used for variable labels, limits
-ih_ddict_post <- httr::POST(
+ih_ddict <- httr::POST(
   url = rc_url,
   body = list(
     token = Sys.getenv("MOSAIC_IH"),
@@ -20,7 +24,78 @@ ih_ddict_post <- httr::POST(
     content = "metadata",          ## export *metadata* (data dictionary)
     format = "csv"                 ## export as *CSV*
   )
+) %>%
+  post_to_df()
+
+## Get vector of date variables (all export in ISO 8601)
+date_vars <- ih_datadict %>%
+  filter(
+    text_validation_type_or_show_slider_number %in% c("date_mdy", "date_ymd")
+  ) %>%
+  pull(field_name)
+
+## -- Export data --------------------------------------------------------------
+## Data collected ONLY on day of enrollment
+day1_df <- post_to_df(
+  httr::POST(
+    url = rc_url,
+    body = list(
+      token = Sys.getenv("MOSAIC_IH"),
+      content = "record",   ## export *records*
+      format = "csv",       ## export as *CSV*
+      ## Q: Which forms to export? A: Those collected *only* day of enrollment
+      forms = paste(
+        c(
+          "enrollment_qualification_form",
+          "contact_information",
+          "prehospital_function_assessment_form",
+          "dates_tracking_form",
+          "enrollment_data_collection_form_mds",
+          "enrollment_nutrition_data_form"
+        ),
+        collapse = ","
+      ),
+      fields = c("id"),     ## additional fields
+      events = "enrollment_trial_d_arm_1", ## baseline visit event only
+      rawOrLabel = "label", ## export factor *labels* vs numeric codes
+      exportCheckboxLabel = TRUE ## export ckbox *labels* vs Unchecked/Checked
+    )
+  )
+) %>%
+  ## Remove any test patients from dataset
+  filter(!str_detect(tolower(id), "test")) %>%
+  ## Convert date/time variables to proper formats
+  mutate_at(vars(one_of(date_vars)), ymd)
+
+################################################################################
+## Enrollment Qualification Form
+################################################################################
+
+## -- Create error codes + corresponding messages for all issues *except* ------
+## -- fields that are simply missing or should fall within specified limits ----
+
+## Codes: Short, like variable names
+## Messages: As clear as possible to the human reader
+
+## tribble = row-wise data.frame; easier to match code + message
+enrqual_codes <- tribble(
+  ~ code,      ~ msg,
+  ## Inclusion criteria
+  "inc_date",  "Inclusion criteria date prior to March 2017 or after date script run",
+  "inc_adult", "Inclusion criteria: adult patient either missing or marked No",
+  "inc_icu",   "Inclusion criteria: in ICU either missing or marked No",
+  "inc_organ", "Inclusion criteria: qualifying organ failure either missing or marked No",
+  "inc_organ_present", "Patient had qualifying organ failure, but no specific organ failures marked present",
+  "inc_mv",    "Patient marked as having both invasive MV and NIPPV at inclusion"
 )
 
-ih_datadict <- post_to_df(ddict_post)
+## Create empty matrix to hold all potential issues
+## Rows = # rows in day1_df; columns = # potential issues
+enrqual_issues <- matrix(
+  FALSE, ncol = nrow(enrqual_codes), nrow = nrow(day1_df)
+)
+colnames(enrqual_issues) <- enrqual_codes$code
+rownames(enrqual_issues) <- with(day1_df, {
+  paste(id, redcap_event_name, sep = '; ') })
 
+## TODO: Check actual issues
